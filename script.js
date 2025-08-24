a702cc23bb26111482567bd27505bb2d702a9d5d
Peter <77168867+peterron03@users.noreply.github.com>
2025-08-24 12:17:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// Get query parameters from the URL
+// Get query parameters
 const urlParams = new URLSearchParams(window.location.search);
 const placeId = urlParams.get('place');
 const jobId = urlParams.get('job');
 
-if (placeId && jobId) {
-  // Build Roblox deep link
+// Function to validate Place ID (must be a positive integer)
+function isValidPlaceId(id) {
+  return /^\d+$/.test(id);
+}
+
+// Function to validate Job ID (must be a 32-character alphanumeric string)
+function isValidJobId(id) {
+  return /^[0-9a-fA-F]{32}$/.test(id);
+}
+
+if (isValidPlaceId(placeId) && isValidJobId(jobId)) {
+  // Build Roblox deep link and redirect
   const robloxUrl = `roblox://experiences/start?placeId=${placeId}&gameInstanceId=${jobId}`;
   window.location.href = robloxUrl;
 } else {
-  document.body.innerHTML = `<p style="font-size:18px;color:red;">
-    Invalid link. Make sure to include <b>place</b> and <b>job</b> parameters.
-  </p>`;
+  // Show error message if invalid
+  document.body.innerHTML = `
+    <p style="font-size:18px; color:red;">
+      Invalid link.<br>
+      Make sure <b>place</b> is a number and <b>job</b> is a valid 32-character ID.
+    </p>
+  `;
 }
